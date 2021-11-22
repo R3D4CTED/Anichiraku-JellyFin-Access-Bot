30f3cfbe80dec27002a0c93b06bfc9eea8f213cf
[REDACTED] <vikee.email@gmail.com>
2021-11-22 11:51:08 +0530

    Add more required configs.

--- config.example.py
+++ config.example.py
@@ -7,3 +7,6 @@ jellyfin_api_key = "" # jellyfin API key stored as string
 
 plex_url = "" # plex base url, stored as string
 plex_api_key = "" # plex API key stored as string
+plex_access_role = 0000000000000000000000 # discord role ID required to access the plex
+guild_id = 0000000000000000000000 # ID of guild
+plex_admin_role = 00000000000000000000 # role required to administrate the plex
